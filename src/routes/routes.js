28f3d455170b1bbe20b52c8403f8e4a83f6b00fd
Mumuni Mohammed <mumunim10@gmail.com>
2021-07-19 16:00:15 +0000

    Started app and added admin layout.

---
+++ src/routes/routes.js
@@ -0,0 +1,10 @@
+import Dashboard from '../back/general/Dashboard';
+import Profile from '../back/general/Profile';
+
+const routes = [
+    {path: '/admin', exact: true, name: 'Admin'},
+    {path: '/admin/dashboard', exact: true, name: 'Dashboard', component: Dashboard},
+    {path: '/admin/profile', exact: true, name: 'Profile', component: Profile},
+]
+
+export default routes;
